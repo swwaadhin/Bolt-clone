3a6bdab04e87853233165fb435c9a1fde8070a8c
swwaadhin <swadhinsahoo2004@gmail.com>
2025-08-13 13:05:36 +0530

    api  added

--- Backend/src/config/environment.ts
+++ Backend/src/config/environment.ts
@@ -1,6 +1,12 @@
-export const config = {
-  port: process.env.PORT || 3000,
-  geminiApiKey: process.env.GEMINI_API_KEY,
-  geminiModel: process.env.GEMINI_MODEL || 'gemini-2.0-flash-lite',
-  nodeEnv: process.env.NODE_ENV || 'Production',
-};
+  console.log(
+  'API Key:',
+  process.env.GEMINI_API_KEY ? 'Loaded' : 'Undefined'
+);
+  
+  
+  export const config = {
+    port: process.env.PORT || 3000,
+    geminiApiKey: process.env.GEMINI_API_KEY,
+    geminiModel: process.env.GEMINI_MODEL || 'gemini-2.0-flash-lite',
+    nodeEnv: process.env.NODE_ENV || 'Production',
+  };
